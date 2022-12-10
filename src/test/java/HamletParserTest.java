e16eea1810e0c712436c9b9f5acde34e2af18ffc
prathibha22 <prccherukuri@gmail.com>
2022-12-10 15:11:29 -0500

    implemented all methods and testmathods passed"

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -12,20 +16,51 @@ public class HamletParserTest {
         this.hamletParser = new HamletParser();
         this.hamletText = hamletParser.getHamletData();
     }
+    @Test
+    public void testFindHamlet() {
+        //Given
+        int expectedCount=472;
+        //When
+        HamletParser objHamletParser=new HamletParser();
+        int actualCount=objHamletParser.findHamlet(hamletText);
+        //Then
+        Assert.assertEquals(expectedCount,actualCount);
+    }
 
     @Test
     public void testChangeHamletToLeon() {
+        //Given
+        int expectedCount=0;
+        //When
+        HamletParser objHamletParser=new HamletParser();
+        String modified=objHamletParser.changeHamletToLeon(hamletText);
+        int actualCount=objHamletParser.findHamlet(modified);
+        //Then
+        Assert.assertEquals(expectedCount,actualCount);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given
+        int expectedCount=0;
+        //When
+        HamletParser objHamletParser=new HamletParser();
+        String modified=objHamletParser.changeHoratioToTariq(hamletText);
+        int actualCount=objHamletParser.findHamlet(modified);
+        //Then
+        Assert.assertEquals(expectedCount,actualCount);
     }
 
     @Test
     public void testFindHoratio() {
+        //Given
+        int expectedCount=158;
+        //When
+        HamletParser objHamletParser=new HamletParser();
+        int actualCount=objHamletParser.findHoratio(hamletText);
+        //Then
+        Assert.assertEquals(expectedCount,actualCount);
     }
 
-    @Test
-    public void testFindHamlet() {
-    }
+
 }
